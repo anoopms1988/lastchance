b25dc08bddc5c9f6ae704bb9c39f1f9cc25a8189
Anoop ms <anoop.msp@tataelxsi.co.in>
2019-02-22 16:58:41 +0530

    CreateAccount

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"lastchance/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.AutoRouter(&controllers.AccountController{})
 }
